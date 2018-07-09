268631a6a3b9d82b76ea39035879bf14d54acce4
Hans-Peter Harmsen <hph@oberon.nl>
2018-07-09 16:42:19 +0200

    Basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hplib',
+      version='0.1',
+      description='Python (3) usefull classes',
+      url='https://github.com/hpharmsen/hplib',
+      author='HP Harmsen',
+      author_email='hp@harmsen.nl',
+      license='GPL',
+      packages=['hplib'],
+      zip_safe=False)
